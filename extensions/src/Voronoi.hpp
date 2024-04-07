7fa8ce645317db38a4d69b1a875d817a6ab90a8f
trevor <trevor@trevg.xyz>
2024-04-07 12:59:05 -0600

    Add voronoi extension code

--- extensions/src/Voronoi.hpp
+++ extensions/src/Voronoi.hpp
@@ -1,21 +1,23 @@
+#include "boost/polygon/point_data.hpp"
 #include "godot_cpp/classes/polygon2d.hpp"
+#include <boost/polygon/voronoi.hpp>
 #include <godot_cpp/classes/control.hpp>
 #include <godot_cpp/classes/wrapped.hpp>
 #include <godot_cpp/variant/typed_array.hpp>
 #include <godot_cpp/variant/vector2.hpp>
-
 #include <vector>
-class Voronoi : public godot::Object {
-  GDCLASS(Voronoi, godot::Object)
+class Voronoi : public godot::Object
+{
+    GDCLASS( Voronoi, godot::Object )
 public:
-  void addVertex(const godot::Point2 &point);
-  void compute();
-  godot::TypedArray<godot::Polygon2D> getPolygons();
+    void addVertex( const godot::Vector2 &vec );
+    void compute();
+    godot::TypedArray<godot::Polygon2D> getPolygons();
 
 private:
-  godot::TypedArray<godot::Polygon2D> polygons_;
-  std::vector<godot::Point2> vertices_;
+    godot::TypedArray<godot::Polygon2D> polygons_;
+    std::vector<boost::polygon::point_data<double>> vertices_;
 
 protected:
-  static void _bind_methods();
+    static void _bind_methods();
 };
