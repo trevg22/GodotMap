24ac801815405076f3aadd5d7f21bcdbcff19524
trevor <trevor@trevg.xyz>
2024-04-12 15:30:47 -0600

    feat start work on bounding infinite segements

--- extensions/src/Voronoi.hpp
+++ extensions/src/Voronoi.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "boost/polygon/point_data.hpp"
+#include "boost/polygon/segment_data.hpp"
 #include "boost/polygon/voronoi_diagram.hpp"
 #include <boost/polygon/voronoi.hpp>
 #include <godot_cpp/classes/control.hpp>
@@ -12,14 +13,37 @@
 #include <vector>
 class Vor
 {
+
 public:
     void addVertex( const boost::polygon::point_data<double> &vertex );
     void compute();
     std::vector<boost::polygon::voronoi_cell<double>> getCells();
-    //todo
-    boost::polygon::voronoi_diagram<double> vd_;
+    // todo
+
 private:
-    std::vector<boost::polygon::point_data<double>> vertices_;
+    typedef double coordinate_type;
+    typedef boost::polygon::point_data<coordinate_type> point_type;
+    typedef boost::polygon::segment_data<coordinate_type> segment_type;
+    typedef boost::polygon::rectangle_data<coordinate_type> rect_type;
+    typedef boost::polygon::voronoi_builder<int> VB;
+    typedef boost::polygon::voronoi_diagram<coordinate_type> VD;
+    typedef VD::cell_type cell_type;
+    typedef VD::cell_type::source_index_type source_index_type;
+    typedef VD::cell_type::source_category_type source_category_type;
+    typedef VD::edge_type edge_type;
+    typedef VD::cell_container_type cell_container_type;
+    typedef VD::cell_container_type vertex_container_type;
+    typedef VD::edge_container_type edge_container_type;
+    typedef VD::const_cell_iterator const_cell_iterator;
+    typedef VD::const_vertex_iterator const_vertex_iterator;
+    typedef VD::const_edge_iterator const_edge_iterator;
+
+    point_type shift_;
+    std::vector<point_type> point_data_;
+    std::vector<segment_type> segment_data_;
+    rect_type brect_;
+    VB vb_;
+    VD vd_;
 };
 class Voronoi : public godot::Object
 {
@@ -27,8 +51,10 @@ class Voronoi : public godot::Object
 public:
     void addVertex( const godot::Vector2 &vertex );
     void compute();
-    static godot::PackedVector2Array toGodotVertices(const boost::polygon::voronoi_cell<double> &cell);
+    static godot::PackedVector2Array toGodotVertices(
+        const boost::polygon::voronoi_cell<double> &cell );
     godot::Array getPolygons();
+    godot::Array getCellCenters();
 
 private:
     godot::Array polygons_;
